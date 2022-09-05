5b65448da3ae1dc8b913d74d97d230e456d827ea
Aryan <aryan4p130202@gmail.com>
2022-09-06 01:16:45 +0530

    array solutions

---
+++ day5-arrays/solution.js
@@ -0,0 +1,69 @@
+// 1. Declare an empty array
+
+// const arr = [];
+// console.log(arr);
+
+// Declare an array with more than 5 number of elements
+// Find the length of your array
+
+// const arr = [1,2,3,4,5,6];
+// console.log('Length: ', arr.length);
+
+// Get the first item, the middle item and the last item of the array
+
+// const arr = [1,2,3,"Aryan",5,6];
+
+// const first = arr[0];
+// const last = arr[5];
+
+// const mid = arr.length / 2;
+
+// console.log('First: ', first);
+// console.log('Mid: ', arr[mid]);
+// console.log('Last: ', last);
+
+// Declare an array called mixedDataTypes, put different data types in the 
+// array and find the length of the array. The array size should be greater than 5
+
+// const mixedDataTypes = ['Aryan', true, 1, 0.1,'Shaw', false, 'pro'];
+
+// const len = mixedDataTypes.length;
+// console.log(len);
+
+// Declare an array variable name itCompanies and assign initial values Facebook, Google, 
+// Microsoft, Apple, IBM, Oracle and Amazon
+// Print the array using console.log()
+// Print the number of companies in the array
+// Print the first company, middle and last company
+// Print out each company
+// Change each company name to uppercase one by one and print them out
+
+const itCompanies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+// console.log(itCompanies);
+// const len = itCompanies.length;
+// console.log(len);
+
+// // first company
+// console.log(itCompanies[0]);
+// // last company
+// console.log(itCompanies[itCompanies.length-1]);
+// // middle company
+// const mid = Math.floor(len/2);
+// // console.log(mid);
+// console.log(itCompanies[mid]);
+
+// for(let i=0 ; i < itCompanies.length; i++){
+//     console.log(itCompanies[i].toUpperCase() + ' ');
+// }
+// Print the array like as a sentence: Facebook, Google, 
+//Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+
+// Method - 1
+
+// let oneLine = itCompanies.toString();
+// console.log(oneLine);
+
+// Method - 2
+// console.log(itCompanies.toString());
+
+
